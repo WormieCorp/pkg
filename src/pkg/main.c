689162e253897796bc204ce54db5ba69f1c66129
AdmiringWorm <kim.nordmo@gmail.com>
2020-05-06 14:36:40 +0200

    feat: add basic program to test parsing

--- src/pkg/main.c
+++ src/pkg/main.c
@@ -1,1 +1,29 @@
-int main(void) { return 0; }
+#include <launchers/launchers.h>
+#include <logging.h>
+#include <malloc.h>
+#include <parsers/parsers.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  ArgumentsData *arguments = parse_arguments(argc - 1, argv + 1);
+
+  char **transformed_args = choco_get_launch_args(arguments);
+
+  printf("Would call 'choco.exe");
+
+  char **pos = transformed_args;
+  while (*pos) {
+    printf(" %s", *pos);
+    free(*pos);
+    *pos++;
+  }
+
+  puts("'\n");
+
+  free(transformed_args);
+
+  release_arguments_data(arguments);
+
+  return 0;
+}
